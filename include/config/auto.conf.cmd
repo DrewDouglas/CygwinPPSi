99fa74028c2f1fb4ca49384992b3e70c0f3913bd
Drew <drew@douglashome.com>
2017-03-22 16:06:24 -0500

    added all files

---
+++ include/config/auto.conf.cmd
@@ -0,0 +1,8 @@
+deps_config := \
+	/home/Drew/PPSi_Cygwin/Kconfig
+
+include/config/auto.conf: \
+	$(deps_config)
+
+
+$(deps_config): ;
---
+++ include/generated/autoconf.h
@@ -0,0 +1,12 @@
+/*
+ * Automatically generated C config: don't edit
+ * Wed Mar 15 20:16:24 2017
+ */
+#define AUTOCONF_INCLUDED
+#define CONFIG_ARCH "cygwin"
+#define CONFIG_ARCH_CFLAGS ""
+#define CONFIG_ARCH_CYGWIN 1
+#define CONFIG_ARCH_LDFLAGS ""
+#define CONFIG_EXT_NONE 1
+#define CONFIG_EXTENSION ""
+#define CONFIG_CROSS_COMPILE ""
